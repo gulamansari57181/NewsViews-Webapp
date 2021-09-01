e6bba76b628eb8fb286bb4753e8e73ce2f5fc04f
gulamansari57181 <gulamansari57181@gmail.com>
2021-09-01 13:23:29 +0530

    Populating news item on screen by looping through sample json news articles  file

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,25 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    return <div>This is news item.</div>;
+    let { title, description, imgUrl, newsUrl } = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
