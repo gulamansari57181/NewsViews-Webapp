c3268fc214f7f410226cea24b45b3c8071da9f46
gulamansari57181 <gulamansari57181@gmail.com>
2021-09-08 21:44:40 +0530

    News.js component updated & News are rendering directly from API not from sample data

--- src/components/News.js
+++ src/components/News.js
@@ -2,45 +2,24 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  // array of articles to used as state
-  articles = [
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
-
   constructor() {
     super();
-    console.log("Hello newsitem components");
+
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
     };
+  }
+
+  // it will execute cute after render has completed
+  async componentDidMount() {
+    // fetching news API
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=5eedf541677b4ab8b82cc1555b88e6e6";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({ articles: parsedData.articles });
   }
 
   render() {
@@ -52,11 +31,13 @@ export class News extends Component {
           {/* Looping through each article of aeticles array and populate the articles */}
           {this.state.articles.map((element) => {
             return (
-              <div className="col-md-4" key={element.url}>
+              <div className="col-md-4 my-3" key={element.url}>
                 <NewsItem
                   // slice method is use to limit the size of characters in title and description
-                  title={element.title.slice(0, 45)}
-                  description={element.description.slice(0, 90)}
+                  title={element.title ? element.title.slice(0, 45) : " "}
+                  description={
+                    element.description ? element.description.slice(0, 90) : " "
+                  }
                   imgUrl={element.urlToImage}
                   newsUrl={element.url}
                 />
